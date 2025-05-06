155cd185f3b8ed1ef7547ab416501879431f3ca2
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-06 23:30:55 +0100

    Update check_file_exist.sh
    Exercice 5

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 # ce code nous si le fichier existe ou non
 if [ -f hello_world.sh ]; then 
-echo "Le fichier 'hello_world' existe"
-else echo "Le fichier 'hello_world' n\'existe pas"
+echo "Le fichier 'hello_world.sh' existe"
+else echo "Le fichier 'hello_world.sh' n\'existe pas"
 fi 
