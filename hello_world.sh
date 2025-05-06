847c7ec0713acac92d9c8d33a38b78b33e8c003e
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-06 13:21:30 +0100

    Update hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#ce code retourne réponse à la question : est-ce que le fichier " hello_world.sh" existe
+#cette commande affiche hello, World !
 
-echo "hello_world"
+echo "Hello, world"
