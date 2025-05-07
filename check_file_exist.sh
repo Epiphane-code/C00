8a2158f9f09c43023ea5bd2839ae16fe6fe79c07
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-07 07:42:17 +0100

    Update check_file_exist.sh
    Exercice 5

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Vérifie si un fichier existe
-
 read -p "Entrez le nom du fichier : " nom
-
 if [ -f "$nom" ]; then
     echo "Le fichier '$nom' existe."
 else
     echo "Le fichier '$nom' n'existe pas."
-fi
+fi 
